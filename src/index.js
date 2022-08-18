342418d986f7d743d410e51fcb744acaa1da321c
Bhaskar <98847406+happy4soul@users.noreply.github.com>
2022-08-18 21:43:25 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentDate = new Date();
+const time = currentDate.getHours();
+var print = "morning";
+var changeColor = {
+  color: "red"
+};
+if (time > 12 && time < 18) {
+  print = "afternoon";
+  changeColor = {
+    color: "green"
+  };
+} else if (time > 18 && time < 24) {
+  print = "night";
+  changeColor = {
+    color: "blue"
+  };
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={changeColor}>
+      {" "}
+      Good {print}{" "}
+    </h1>
+  </div>,
+
+  document.querySelector("#root")
+);
